e9d20892f7fc30b15f6287a48ad6a73834d6941c
codedbychavez <codedbychavez@gmail.com>
2022-10-21 23:02:59 -0300

    Initial commit

---
+++ ab-testing-ios-sample/ab-testing-ios-sample/ab_testing_ios_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ab_testing_ios_sampleApp.swift
+//  ab-testing-ios-sample
+//
+//  Created by Chavez Harris on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ab_testing_ios_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
